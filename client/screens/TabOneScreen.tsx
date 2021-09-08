bd79a92b761f15568ce3e594db75106e298f8ea8
hheiyanthuduwa <hheiyanthuduwa@zapmii.com>
2021-09-08 20:54:51 +0530

    add tailwind css

--- client/screens/TabOneScreen.tsx
+++ client/screens/TabOneScreen.tsx
@@ -4,13 +4,11 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-
+import tailwind from 'tailwind-rn';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
+    <View style={{ ...tailwind(`bg-red-200 flex`), ...styles.container }}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
