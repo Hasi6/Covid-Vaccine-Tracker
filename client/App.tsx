b7bb127112f0305b6d4131bf594908394a9237c3
Hasi6 <hasitha.chandula@gmail.com>
2021-10-09 22:12:41 +0530

    updated

--- client/App.tsx
+++ client/App.tsx
@@ -13,7 +13,32 @@ import {
   Poppins_300Light,
 } from '@expo-google-fonts/poppins';
 import AlertComponent from './src/components/Alert/Alert';
+import OneSignal from 'react-native-onesignal';
+//OneSignal Init Code
+OneSignal.setLogLevel(6, 0);
+OneSignal.setAppId('0cbdbf44-814f-4ce0-9c60-ed48ea770ab4');
+//END OneSignal Init Code
 
+//Prompt for push on iOS
+OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+  console.log('Prompt response:', response);
+});
+
+//Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent) => {
+  console.log('OneSignal: notification will show in foreground:', notificationReceivedEvent);
+  let notification = notificationReceivedEvent.getNotification();
+  console.log('notification: ', notification);
+  const data = notification.additionalData;
+  console.log('additionalData: ', data);
+  // Complete with null means don't show a notification.
+  notificationReceivedEvent.complete(notification);
+});
+
+//Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler((notification) => {
+  console.log('OneSignal: notification opened:', notification);
+});
 const App = () => {
   let [fontsLoaded] = useFonts({
     Poppins_600SemiBold,
